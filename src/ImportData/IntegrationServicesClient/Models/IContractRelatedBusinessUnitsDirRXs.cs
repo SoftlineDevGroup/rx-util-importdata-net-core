57afd261622be37c49df08daf191ac6ed903925e
Garifullin_AF <Garifullin_AF@directum.ru>
2022-08-16 15:43:39 +0300

    Промежуточный, для фиксации изменений

---
+++ src/ImportData/IntegrationServicesClient/Models/IContractRelatedBusinessUnitsDirRXs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImportData.IntegrationServicesClient.Models
+{
+    [EntityName("RelatedBusinessUnitsDirRX")]
+    class IContractRelatedBusinessUnitsDirRXs
+    {        
+        public IBusinessUnits BusinessUnit { get; set; }
+        public IContracts Contract { get; set; }
+        public IEmployees Employee { get; set; }
+    }
+}
